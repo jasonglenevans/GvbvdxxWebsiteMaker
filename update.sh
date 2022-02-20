ae320cd69f152277bc473c26f528f852e89012ef
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2022-02-19 20:55:49 -0500

    Upload From Git Bash

---
+++ update.sh
@@ -0,0 +1,9 @@
+git init
+git add .
+git commit -m "Upload From Git Bash"
+git branch -M main
+git remote add origin https://github.com/jasonglenevans/GvbvdxxWebsiteMaker.git
+git push -u origin main
+
+while 1 (
+)
